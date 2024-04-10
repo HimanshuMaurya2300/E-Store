daa8bbf2ee790f5a1cdb8db9d78aaeb16408c2ab
Himanshu <mauryah380@gmail.com>
2024-04-10 09:10:33 +0530

    build the strapi server

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,16 +1,16 @@
-const cronTasks = require('./cron-tasks');
+const cronTasks = require('./cron-tasks.js');
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: {
+    enable: true,
+    tasks: cronTasks,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  cron: {
-    enable: true,
-    tasks: cronTasks
-  }
 });
